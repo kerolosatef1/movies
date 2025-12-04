f2210e746855e080f3c1f67db41015f6c7fc5a47
kerolosatef1 <atefkerolos25@gmail.com>
2025-12-04 03:12:01 +0200

    Done

--- src/components/Pagination/Pagination.tsx
+++ src/components/Pagination/Pagination.tsx
@@ -7,25 +7,25 @@ export default function Pagination({getPage ,pageCount}: { getPage: (page: numbe
 
   return (
     <ReactPaginate
-      breakLabel="..."
-      nextLabel="التالي >"
-      onPageChange={handlePageClick}
-      pageRangeDisplayed={2}
-      marginPagesDisplayed={2}
-      pageCount={pageCount}
-      previousLabel="< السابق"
-      renderOnZeroPageCount={null}
-      containerClassName="flex justify-center items-center space-x-2 mt-5"
-      pageClassName="rounded overflow-hidden"
-      pageLinkClassName="bg-gray-200 text-gray-700 px-3 py-1 block hover:bg-gray-300 cursor-pointer"
-      previousClassName="rounded overflow-hidden"
-      previousLinkClassName="bg-gray-200 text-gray-700 px-3 py-1 block hover:bg-gray-300 cursor-pointer"
-      nextClassName="rounded overflow-hidden"
-      nextLinkClassName="bg-gray-200 text-gray-700 px-3 py-1 block hover:bg-gray-300 cursor-pointer"
-      breakClassName="rounded overflow-hidden"
-      breakLinkClassName="px-3 py-1 block"
-      activeClassName="bg-red-600"
-      activeLinkClassName="bg-red-600 text-white px-3 py-1 "
-    />
+  breakLabel="..."
+  nextLabel="التالي >"
+  onPageChange={handlePageClick}
+  pageRangeDisplayed={window.innerWidth < 640 ? 1 : 2}
+  marginPagesDisplayed={1}
+  pageCount={pageCount}
+  previousLabel="< السابق"
+  renderOnZeroPageCount={null}
+  containerClassName="flex flex-wrap justify-center items-center gap-2 mt-5"
+  pageClassName="rounded overflow-hidden"
+  pageLinkClassName="bg-gray-200 text-gray-700 px-2 py-1 sm:px-3 sm:py-1 block hover:bg-gray-300 cursor-pointer"
+  previousClassName="rounded overflow-hidden"
+  previousLinkClassName="bg-gray-200 text-gray-700 px-2 py-1 sm:px-3 sm:py-1 block hover:bg-gray-300 cursor-pointer"
+  nextClassName="rounded overflow-hidden"
+  nextLinkClassName="bg-gray-200 text-gray-700 px-2 py-1 sm:px-3 sm:py-1 block hover:bg-gray-300 cursor-pointer"
+  breakClassName="rounded overflow-hidden"
+  breakLinkClassName="px-2 py-1 sm:px-3 sm:py-1 block"
+  activeClassName="bg-red-600"
+  activeLinkClassName="bg-red-600 text-white px-2 py-1 sm:px-3 sm:py-1"
+/>
   );
 }
